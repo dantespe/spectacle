415f66216a552dc1b0f5be2ee3b81275e2814708
Dante Spencer <dantespe@umich.edu>
2023-05-01 16:29:45 -0700

    Update CreateDatasetResponse to include information about DisplayName, etc.

--- manager/response.go
+++ manager/response.go
@@ -1,146 +1,150 @@
 package manager
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 
-	"github.com/dantespe/spectacle/dataset"
+    "github.com/dantespe/spectacle/dataset"
 )
 
 type Response interface {
-	JSON() map[string]interface{}
-	ResponseCode() int
+    JSON() map[string]interface{}
+    ResponseCode() int
 }
 
 // DefaultResponse
 type DefaultResponse struct {
-	Response
+    Response
 }
 
 func (*DefaultResponse) JSON() map[string]string {
-	return make(map[string]string)
+    return make(map[string]string)
 }
 
 func (*DefaultResponse) ResponseCode() int  {
-	return http.StatusOK
+    return http.StatusOK
 }
 
 // StatusResponse
 type StatusResponse struct {
-	DefaultResponse
+    DefaultResponse
 
-	Error error
-	NumRecords int
+    Error error
+    NumRecords int
 }
 
 func (r *StatusResponse) JSON() map[string]string {
-	if r.Error != nil {
-		return map[string]string{
-			"status": "UNHEALTHY",
-		}
-	}
-	return map[string]string{
-		"status": "HEALTHY",
-		"num_records": fmt.Sprintf("%d", r.NumRecords),
-	}
+    if r.Error != nil {
+        return map[string]string{
+            "status": "UNHEALTHY",
+        }
+    }
+    return map[string]string{
+        "status": "HEALTHY",
+        "num_records": fmt.Sprintf("%d", r.NumRecords),
+    }
 }
 
 func (r *StatusResponse) ResponseCode() int  {
-	return http.StatusOK
+    return http.StatusOK
 }
 
 // CreateDatasetResponse 
 type CreateDatasetResponse struct {
-	DefaultResponse
+    DefaultResponse
 
-	Error error
-	DatasetId uint64
+    Error error
+    DatasetId uint64
+    DatasetUrl string
+    DisplayName string
 }
 
-func (r *CreateDatasetResponse) JSON() map[string]string {
-	if r.Error != nil {
-		return map[string]string{
-			"message": "Failed to create dataset.",
-		}
-	}
-	return map[string]string{
-		"DatasetId": fmt.Sprintf("%d", r.DatasetId),
-	}	
+func (r *CreateDatasetResponse) JSON() map[string]interface{} {
+    if r.Error != nil {
+        return map[string]interface{}{
+            "message": "Failed to create dataset.",
+        }
+    }
+    return map[string]interface{}{
+        "DatasetUrl": r.DatasetUrl,
+        "DatasetId": r.DatasetId, 
+        "DisplayName": r.DisplayName,
+    }	
 }
 
 func (r *CreateDatasetResponse) ResponseCode() int {
-	if r.Error != nil {
-		return http.StatusInternalServerError
-	}
-	return http.StatusCreated
+    if r.Error != nil {
+        return http.StatusInternalServerError
+    }
+    return http.StatusCreated
 }
 
 // GetDatasetResponse
 type GetDatasetResponse struct {
-	DefaultResponse
+    DefaultResponse
 
-	Error error
-	Dataset *dataset.Dataset
-	DatasetId uint64
-	DisplayName string
-	NumRecords uint64
+    Error error
+    Dataset *dataset.Dataset
+    DatasetId uint64
+    DisplayName string
+    NumRecords uint64
 }
 
 func (r *GetDatasetResponse) JSON() map[string]string {
-	if r.Error != nil {
-		return map[string]string {
-			"message": "Dataset not found.",
-		}
-	}
+    if r.Error != nil {
+        return map[string]string {
+            "message": "Dataset not found.",
+        }
+    }
 
-	return map[string]string {
-		"DatasetId": fmt.Sprintf("%d", r.Dataset.Id),
-		"DisplayName": r.Dataset.DisplayName,
-		"NumRecords": fmt.Sprintf("%d", r.Dataset.NumRecords), 
-	}
+    return map[string]string {
+        "DatasetId": fmt.Sprintf("%d", r.Dataset.Id),
+        "DisplayName": r.Dataset.DisplayName,
+        "NumRecords": fmt.Sprintf("%d", r.Dataset.NumRecords), 
+    }
 }
 
 func (r *GetDatasetResponse) ResponseCode() int {
-	if r.Error != nil {
-		return http.StatusNotFound
-	}
-	return http.StatusOK
+    if r.Error != nil {
+        return http.StatusNotFound
+    }
+    return http.StatusOK
 }
 
 // ListDatasetsResponse
 type ListDatasetsResponse struct {
-	DefaultResponse
+    DefaultResponse
 
-	Error error
-	Results []interface{}
+    Error error
+    Results []interface{}
 
-	// Page int
-	// Next string
-	TotalDatasets int
+    // Page int
+    // Next string
+    TotalDatasets int
 }
 
 func newListDatasetsResponse() *ListDatasetsResponse {
-	return &ListDatasetsResponse{
-		Results: make([]interface{}, 0),
-	}
+    return &ListDatasetsResponse{
+        Results: make([]interface{}, 0),
+    }
 }
 
 func (r *ListDatasetsResponse) JSON() map[string]interface{} {
-	if r.Error != nil {
-		return map[string]interface{} {
-			"message": "Failed to list datasets.",
-		}
-	}
+    if r.Error != nil {
+        return map[string]interface{} {
+            "message": "Failed to list datasets.",
+        }
+    }
 
-	return map[string]interface{} {
-		"Results": r.Results,
-		"TotalDatasets": len(r.Results),
-	}
+    return map[string]interface{} {
+        "Results": r.Results,
+        "TotalDatasets": len(r.Results),
+    }
 }
 
 func (r *ListDatasetsResponse) ResponseCode() int {
-	if r.Error != nil {
-		return http.StatusBadRequest
-	}
-	return http.StatusOK
+    if r.Error != nil {
+        return http.StatusBadRequest
+    }
+    return http.StatusOK
 }
