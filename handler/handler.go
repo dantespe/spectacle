0d34e44d75285239646689354503fc853989e8ba
Dante Spencer <dantespe@umich.edu>
2023-04-30 17:00:45 -0700

    Refactor server.go by moving logic to package handler

---
+++ handler/handler.go
@@ -0,0 +1,97 @@
+// Package handler contains logic from translating web requests the spectacle backend.package handler
+package handler
+
+import (
+    "log"
+    "net/http"
+    "github.com/dantespe/spectacle/manager"
+    "github.com/gin-gonic/gin"  	
+)
+
+type RestHandler struct {
+    mgr *manager.Manager
+    rb *manager.RequestBuilder
+}
+
+func NewRestHandler() (*RestHandler, error) {
+    mgr, err := manager.New()
+    if err != nil {
+        return nil, err
+    }
+
+    return &RestHandler{
+        mgr: mgr,
+        rb: &manager.RequestBuilder{},
+    }, nil
+}
+
+func (h *RestHandler) Status(c *gin.Context) {
+    resp := h.mgr.Status()
+    c.JSON(resp.ResponseCode(), resp.JSON())
+}
+  
+func (h *RestHandler) CreateDataset(c *gin.Context) { 
+    resp := h.mgr.CreateDataset()
+    c.JSON(resp.ResponseCode(), resp.JSON())
+}
+  
+func (h *RestHandler) GetDataset(c *gin.Context)  {
+    req, err := h.rb.GetDatasetRequestBuilder(c)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, map[string]string{
+        "message": err.Error(),
+        })
+        return
+    }
+    resp := h.mgr.GetDataset(req)
+    c.JSON(resp.ResponseCode(), resp.JSON())
+}
+  
+func (h *RestHandler) ListDatasets(c *gin.Context) {
+    req, err := h.rb.ListDatasetsRequestBuilder(c)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, map[string]string{
+        "message": err.Error(),
+        })
+        return
+    }
+    resp := h.mgr.ListDatasets(req)
+    c.JSON(resp.ResponseCode(), resp.JSON())
+}
+  
+func (h *RestHandler) UploadDataset(c *gin.Context)  {
+    resp, err := h.rb.UploadDatasetRequestBuilder(c)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, map[string]string{
+        "message": err.Error(),
+        })
+    }
+  
+    _, header, err := c.Request.FormFile("file")
+    if err != nil {
+      c.JSON(http.StatusBadRequest, map[string]string{
+        "message": err.Error(),
+      })
+    }
+    log.Printf("Reading filename: %s", header.Filename)
+
+    c.JSON(200, map[string]interface{}{
+      "filename": header.Filename,
+      "resp": resp,
+    })
+}
+
+func (h *RestHandler) GetRoutes() map[string]gin.HandlerFunc {
+    return map[string]gin.HandlerFunc {
+        "/status": h.Status,
+        "/datasets": h.ListDatasets,
+        "/dataset/:id": h.GetDataset,
+    }
+}
+
+func (h *RestHandler) PostRoutes() map[string]gin.HandlerFunc {
+    return map[string]gin.HandlerFunc {
+        "/dataset": h.CreateDataset,
+        // "/datasets/:id/upload": h.UploadDataset,
+    }
+}
--- server.go
+++ server.go
@@ -2,25 +2,29 @@
 package main
 
 import (
-  "net/http"
+  "log"
 
+  "github.com/dantespe/spectacle/handler"
   "github.com/gin-gonic/gin"
 )
 
-func StatusHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "HEALTHY",
-	})
-}
-
-func CreateDatasetHandler(c *gin.Context) {
-}
-
-
 func main() {
-  r := gin.Default()
-
-  r.GET("/status", StatusHandler)
-
-  r.Run() // localhost:8080
+    // TODO: Add UI 
+    router := gin.Default()
+    
+    rh, err := handler.NewRestHandler()
+    if err != nil {
+        log.Fatal(err)
+    }
+    rest := router.Group("rest")
+    {
+        for k,v := range rh.GetRoutes() {
+            rest.GET(k, v)
+        } 
+        for k,v := range rh.PostRoutes() {
+            rest.POST(k, v)
+        }
+    }
+    // TODO: Add UI Handler 
+    router.Run() // localhost:8080
 }
