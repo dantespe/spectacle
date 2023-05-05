5eae0787c47411b5ea103d7108b6348ef1afaec6
Tia Burton <tiaaburton@me.com>
2023-05-04 22:52:37 -0700

    Adding basic bootstrap UI.

---
+++ handler/ui.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/dantespe/spectacle/manager"
+	"github.com/gin-gonic/gin"
+)
+
+type UIHandler struct {
+	mgr *manager.Manager
+	rb  *manager.RequestBuilder
+}
+
+func NewUIHandler() (*UIHandler, error) {
+	mgr, err := manager.New()
+	if err != nil {
+		return nil, err
+	}
+
+	return &UIHandler{
+		mgr: mgr,
+		rb:  &manager.RequestBuilder{},
+	}, nil
+}
+
+func (u *UIHandler) Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"img_source": "/assets/images/spectacle.png",
+	})
+}
+
+func (u *UIHandler) Starter(c *gin.Context) {
+	c.HTML(http.StatusOK, "get_started.html", nil)
+}
+
+// func Home() any {
+// 	return map[string]int{"datasetId": 9908576756734740233}
+// }
+
+func (u *UIHandler) GetRoutes() map[string]gin.HandlerFunc {
+	return map[string]gin.HandlerFunc{
+		"/welcome":     u.Index,
+		"/get_started": u.Starter,
+		// "/login":    u.ListDatasets,
+		// "/settings": u.GetDataset,
+	}
+}
+
+func (u *UIHandler) PostRoutes() map[string]gin.HandlerFunc {
+	return map[string]gin.HandlerFunc{
+		// "/create_chart": ...
+	}
+}
--- server.go
+++ server.go
@@ -2,29 +2,54 @@
 package main
 
 import (
-  "log"
+	"log"
+	"os"
 
-  "github.com/dantespe/spectacle/handler"
-  "github.com/gin-gonic/gin"
+	"github.com/dantespe/spectacle/handler"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    // TODO: Add UI 
-    router := gin.Default()
-    
-    rh, err := handler.NewRestHandler()
-    if err != nil {
-        log.Fatal(err)
-    }
-    rest := router.Group("rest")
-    {
-        for k,v := range rh.GetRoutes() {
-            rest.GET(k, v)
-        } 
-        for k,v := range rh.PostRoutes() {
-            rest.POST(k, v)
-        }
-    }
-    // TODO: Add UI Handler 
-    router.Run() // localhost:8080
+	// TODO: Add UI
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var assets_folder string = wd + "/assets"
+
+	router.Static("assets", assets_folder)
+
+	rh, err := handler.NewRestHandler()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rest := router.Group("rest")
+	{
+		for k, v := range rh.GetRoutes() {
+			rest.GET(k, v)
+		}
+		for k, v := range rh.PostRoutes() {
+			rest.POST(k, v)
+		}
+	}
+
+	uh, uiErr := handler.NewUIHandler()
+	if uiErr != nil {
+		log.Fatal(uiErr)
+	}
+	ui := router.Group("home")
+	{
+		for k, v := range uh.GetRoutes() {
+			ui.GET(k, v)
+		}
+		for k, v := range uh.PostRoutes() {
+			ui.POST(k, v)
+		}
+	}
+	// TODO: Add UI Handler
+	router.Run() // localhost:8080
 }
